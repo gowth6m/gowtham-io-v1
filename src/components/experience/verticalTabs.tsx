c3cbcebe0f7b90c22c4e18edd22272b0db8bbc20
Gowtham Ravindrathas <39655386+gowth6m@users.noreply.github.com>
2024-12-13 10:27:02 +0000

    Update verticalTabs.tsx

--- src/components/experience/verticalTabs.tsx
+++ src/components/experience/verticalTabs.tsx
@@ -8,7 +8,8 @@ export default function VerticalTabs() {
   const [categories] = useState([
     "HIVED",
     "UNIVERSITY OF EXETER",
-    "SWMSTUDIOS",
+    "EUROPEAN SPACE AGENCY (ESA)",
+    "HATLESS STUDIOS",
     "NOCXA",
   ]);
 
@@ -130,7 +131,7 @@ const experience: TabContent[] = [
     role: "Frontend Developer",
     company: "HIVED",
     link: "https://hived.space",
-    dates: "Feb 2023 - Present",
+    dates: "Jan 2023 - Present",
     descriptions: [
       "Lead frontend development at HIVED",
       "Create and implement solutions for a variety of microservices used by HIVED",
@@ -150,11 +151,11 @@ const experience: TabContent[] = [
     role: "Software Engineer",
     company: "University of Exeter",
     link: "https://www.exeter.ac.uk/",
-    dates: "May 2022 - Feb 2023",
+    dates: "Jan 2022 - Jan 2023",
     descriptions: [
+      "Work on a variety of microservices used within the university. Work with Flutter for mobile apps and React for web apps.",
       "Work on the University of Exeter app that is used by around 22,000 students",
       "Create chatbot API microservice used in various applications",
-      "Create virtual tour app and mini games for freshers events",
     ],
     technologies: [
       "React",
@@ -167,14 +168,42 @@ const experience: TabContent[] = [
       "CDK",
     ],
   },
-  {
-    role: "Full Stack Developer",
-    company: "Swmstudios",
-    link: "https://swmstudios.com/",
-    dates: "Mar 2021 - Nov 2021",
+    {
+    role: "Backend Engineer",
+    company: "European Space Agency (ESA)",
+    link: "https://www.esa.int/",
+    dates: "Jun 2021 - Jan 2022",
     descriptions: [
-      "Design and develop websites for customers",
-      "Develop and maintain internal tools",
+      "Led backend development for the interactive virtual tour software for ESA's New Norcia Ground Station in Australia to help ESA staff visualise and operate the site remotely from Europe.",
+    ],
+    technologies: [
+      "React",
+      "Flutter",
+      "Node.js",
+      "TypeScript",
+      "AWS",
+      "Fargate",
+      "VPC",
+      "Route53",
+      "DynamoDB",
+      "IAM",
+      "TypeScript",
+      "CDK",
+      "Postgres",
+      "Docker",
+      "Kubernetes",
+      "SQS",
+      "SNS",
+      "RDS",
+    ],
+  },
+  {
+    role: "Software Engineer",
+    company: "Hatless Studios",
+    link: "https://www.linkedin.com/company/hatless-studios",
+    dates: "Oct 2019 - Jun 2021",
+    descriptions: [
+      "Work on a variety of projects on both frontend and backend in various industries such as education, health and finance. Worked on inventory management system, student app for university and internal tools and analytics dashboards for agencies.",
     ],
     technologies: ["React", "Next", "Framer motion", "TypeScript"],
   },
